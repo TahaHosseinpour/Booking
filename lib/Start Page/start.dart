603f97a62b674319eb48b42c5900bdfa79821c91
s.h.m <s.h.mostafavi82@gmail.com>
2023-05-23 14:07:16 +0330

    start widget

---
+++ lib/Start Page/start.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class start extends StatelessWidget {
+  const start({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              child: Image.asset(
+                "assets/images/earth.png",
+              ),
+            ),
+            const SizedBox(height: 50),
+            const Text(
+              "LOGO",
+              style: TextStyle(
+                fontSize: 34,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "Booking Any Where",
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "Flight , Train , Bus",
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                  fixedSize: const Size(358, 100),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(70),
+                  )),
+              child: const Text(
+                "Let's Start",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontFamily: 'Poppins',
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            const Text(
+              "version 1.0",
+              style: TextStyle(
+                fontSize: 15,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
