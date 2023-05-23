8afddf2225b39bd011191ea270ce6cf5271a82af
Taha Hosseinpour <t.hosseinpour2347@gmail.com>
2023-05-23 09:12:04 +0330

    add colors file

---
+++ lib/Information/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final Color yellow1 = Color.fromARGB(255, 255, 216, 0);
+final Color yellow2 = Color.fromARGB(255, 255, 216, 0);
+
+final Color green1 = Color.fromARGB(255, 208, 239, 218);
+final Color green2 = Color.fromARGB(255, 111, 185, 143);
+final Color green3 = Color.fromARGB(255, 44, 120, 115);
+
+final Color grey1 = Color.fromARGB(255, 198, 198, 198);
