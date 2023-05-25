c53ecdbad7c176690e1e337cb487f1dacd2556ce
Taha Hosseinpour <t.hosseinpour2347@gmail.com>
2023-05-25 23:41:52 +0330

    complete two_button.dart pare shodam :)

--- lib/Information/colors.dart
+++ lib/Information/colors.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 final Color yellow1 = Color.fromARGB(255, 255, 216, 0);
-final Color yellow2 = Color.fromARGB(255, 255, 216, 0);
+final Color yellow2 = Color.fromARGB(255, 255, 240, 142);
 
 final Color green1 = Color.fromARGB(255, 208, 239, 218);
 final Color green2 = Color.fromARGB(255, 111, 185, 143);
 final Color green3 = Color.fromARGB(255, 44, 120, 115);
 
 final Color grey1 = Color.fromARGB(255, 198, 198, 198);
+final Color grey2 = Color.fromARGB(255, 135, 135, 135);
+
 
 final Color myColor = Color.fromARGB(255, 255, 216, 0); // رنگ HEX شما
 final MaterialColor mySwatch = MaterialColor(myColor.value, {
