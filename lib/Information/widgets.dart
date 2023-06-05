8b511d5ded9c05721cc2d2b34ef608e8bb7d9d32
Taha Hosseinpour <t.hosseinpour2347@gmail.com>
2023-06-05 22:16:52 +0330

    update buildNavigationBar

--- lib/Information/widgets.dart
+++ lib/Information/widgets.dart
@@ -1,35 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:booking/Information/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-Container buildNavigationBar(BuildContext context) {
+ClipRRect buildNavigationBar(BuildContext context) {
   final screenWidth = MediaQuery.of(context).size.width;
   final screenHeight = MediaQuery.of(context).size.height;
-  return Container(
-    color: green2,
-    padding:  EdgeInsets.only(top: screenHeight * 0.025,bottom: screenHeight * 0.025),
-    child : Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        SvgPicture.asset(
-          'assets/images/ticket.svg',
-          height: screenHeight * 0.036,
-          width: screenWidth * 0.076,
-          color: Colors.white,
-        ),
-        SvgPicture.asset(
-          'assets/images/home.svg',
-          height: screenHeight * 0.041,
-          width: screenWidth * 0.089,
-          color: green3,
-        ),
-        SvgPicture.asset(
-          'assets/images/setting.svg',
-          height: screenHeight * 0.036,
-          width: screenWidth * 0.069,
-          color: Colors.white,
-        ),
-      ],
+  return ClipRRect(
+    borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(25),
+      topRight: Radius.circular(25)
+    ),
+    child: Container(
+      color: green2,
+      padding:  EdgeInsets.only(top: screenHeight * 0.025,bottom: screenHeight * 0.025),
+      child : Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          InkWell(
+            child: SvgPicture.asset(
+              'assets/images/profile.svg',
+              height: screenWidth * 0.079,
+              width: screenWidth * 0.083,
+              color: Colors.white,
+            ),
+            onTap: (){},
+          ),
+          InkWell(
+            child: SvgPicture.asset(
+              'assets/images/transaction.svg',
+              height: screenWidth * 0.079,
+              width: screenWidth * 0.083,
+              color: Colors.white,
+            ),
+            onTap: (){},
+          ),
+          InkWell(
+            child: SvgPicture.asset(
+              'assets/images/ticket.svg',
+              height: screenWidth * 0.079,
+              width: screenWidth * 0.083,
+              color: Colors.white,
+            ),
+            onTap: (){},
+          ),
+          InkWell(
+            child: SvgPicture.asset(
+              'assets/images/home.svg',
+              height: screenWidth * 0.079,
+              width: screenWidth * 0.083,
+              color: Colors.white,
+            ),
+            onTap: (){},
+          ),
+        ],
+      ),
     ),
   );
 }
