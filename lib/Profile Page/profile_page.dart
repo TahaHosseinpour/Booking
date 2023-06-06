0b3bbf71319082304b009f0298cec23befcf1ef9
s.h.m <s.h.mostafavi82@gmail.com>
2023-06-06 13:38:26 +0330

    update

--- lib/Profile Page/profile_page.dart
+++ lib/Profile Page/profile_page.dart
@@ -1,3 +1,5 @@
+import 'package:booking/Information/wallet.dart';
+import 'package:booking/Profile%20Page/shapes_painter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -33,8 +35,39 @@ class _ProfilePageState extends State<ProfilePage> {
               child: Container(
                 width: screenWidth,
                 height: screenHeight * 0.37,
-                child:Text(
-                  "salam"
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(right: screenWidth * 0.08, left: screenWidth * 0.08, top: screenHeight * 0.032),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          InkWell(
+                            child: Container(
+                              child: SvgPicture.asset('assets/images/profile_edit.svg',
+                              height: screenHeight * 0.139,
+                                width: screenWidth * 0.3,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: screenHeight * 0.048,
+                            child:FittedBox(
+                              child:  Text("T.hosseinpour",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            )
+                          )
+                        ],
+                      ),
+                    ),
+                    Wallet(balance: balance)
+                  ],
                 )
               ),
             )
@@ -46,23 +79,3 @@ class _ProfilePageState extends State<ProfilePage> {
 }
 
 
-
-const double _kCurveHeight = 35;
-
-class ShapesPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final p = Path();
-    p.lineTo(0, size.height - _kCurveHeight);
-    p.relativeQuadraticBezierTo(size.width / 2, 2 * _kCurveHeight, size.width, 0);
-    p.lineTo(size.width, 0);
-    p.close();
-
-    canvas.drawPath(p, Paint()..color = yellow2);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
-}
