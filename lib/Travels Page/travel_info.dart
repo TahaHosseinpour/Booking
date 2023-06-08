ef3f61be089feb19ed4964fc8fefcb2e0d038b95
Taha Hosseinpour <t.hosseinpour2347@gmail.com>
2023-06-08 13:59:45 +0330

    remove flutter_screenutil library

--- lib/Travels Page/travel_info.dart
+++ lib/Travels Page/travel_info.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../Information/colors.dart';
